237d1522a1e2015c9a059f0411de87c4c611b60e
DJG-coderslab <coderslab.sjg@pwr.pl>
2021-04-26 21:11:58 +0200

    Add static files and base template

--- training/views.py
+++ training/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views import View
 
 # from training.forms import QuestionForm
-# Create your views here.
+from training.models import Question
 
 
+class Tmp(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'question': Question.objects.all().order_by('?').first()
+        }
+        return render(request, 'training/tmp.html', context=context)
