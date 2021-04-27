33fc7dbc44b8bdb9ad5950b779bb6663e270b649
JG <coderslab.sjg@pwr.pl>
2021-04-27 13:06:10 +0200

    In progress...

--- training/views.py
+++ training/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
-# from training.forms import QuestionForm
+# from training.forms import
 from training.models import Question
 
 
 class Tmp(View):
     def get(self, request, *args, **kwargs):
+        questions = Question.objects.all().order_by('?').first()
         context = {
-            'question': Question.objects.all().order_by('?').first()
+            'question': questions,
         }
         return render(request, 'training/tmp.html', context=context)
