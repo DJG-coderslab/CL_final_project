1e8bb5e06b59b6bebd955e9c4aebceed8e9b6f85
DJG-coderslab <coderslab.sjg@pwr.pl>
2021-04-28 19:51:36 +0200

    Create form for registering employee

--- training/forms.py
+++ training/forms.py
@@ -1,9 +1,12 @@
 
 from django import forms
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
 from training.models import Answer, Question
 
+
+User = get_user_model()
 
 class AnswerForm(forms.ModelForm):
     """Form for Answer model, using in admin panel"""
@@ -18,3 +21,21 @@ class QuestionF(forms.ModelForm):
         model = Question
         fields = ['content', 'points']
 
+
+class UserRegisterForm(forms.ModelForm):
+    """Based by auth.user form to register employee"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = '8000511'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Jan'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Nowak'
+       
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name',)
+        labels = {
+            'username': 'numer pracownika',
+            'first_name': 'imię',
+            'last_name': 'nazwisko'
+        }
+        
--- training/views.py
+++ training/views.py
@@ -4,4 +4,11 @@ from django.views import View
 # from training.forms import QuestionForm
 # Create your views here.
 
+from training.forms import UserRegisterForm
 
+class Tmp(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'form': UserRegisterForm()
+        }
+        return render(request, 'training/tmp.html', context=context)
